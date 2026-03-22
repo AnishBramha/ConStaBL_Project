7eb66287193c6eb1dc637f3464f8ccd5d5207c9e
AnishBramha <Anish.Teja@iiitb.ac.in>
2026-03-22 21:40:25 +0530

    Configured classpath and project root

--- week-1/anish/SubwayTurnstile/build.sh
+++
@@ -1,6 +0,0 @@
-javac -d bin -cp ".:lib/commons-scxml-0.9.jar:lib/commons-jexl-1.1.jar:lib/commons-logging-1.2.jar:lib/commons-digester-1.8.jar:lib/commons-beanutils-1.7.0.jar:lib/xalan-2.7.2.jar:lib/serializer-2.7.2.jar" src/TurnstileGUI.java
-
-java -cp "bin:lib/commons-scxml-0.9.jar:lib/commons-jexl-1.1.jar:lib/commons-logging-1.2.jar:lib/commons-digester-1.8.jar:lib/commons-beanutils-1.7.0.jar:lib/xalan-2.7.2.jar:lib/serializer-2.7.2.jar" TurnstileGUI
-
-
-
